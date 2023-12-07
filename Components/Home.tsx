fb72af83c29d38d86f7955419dee574c76a7dcc8
AllGamers20 <husseinmoubarackmonster@gmail.com>
2023-12-07 18:19:17 +0200

    modify

--- Components/Home.tsx
+++ Components/Home.tsx
@@ -5,7 +5,7 @@ import Call from './Call';
 
 const Home = () => {
   return (
-    <div className='w-full h-screen flex items-start justify-center bg-main/60'>
+    <div className='w-full h-screen flex items-start justify-center bg-main/60 md:w-full'>
       <div className='flex flex-col items-center justify-start'>
         <Logo />
         <Button/>
